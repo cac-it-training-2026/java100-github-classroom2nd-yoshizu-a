e06724a3db195e2a572fadbd5417366571e52cf6
yoshizu <yoshizu@YOSHIZU-AEROG11.cac-k.jp.local>
2026-04-15 10:18:01 +0900

    lesson01-challenge02 完了

--- Java100_questions_cac2nd/src/lesson01/challenge02/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge02/Patisserie.java
@@ -19,5 +19,12 @@
 package lesson01.challenge02;
 
 public class Patisserie {
+	public static void main(String[] args) {
+		System.out.println("本日のおすすめ商品です。");
+		System.out.println(" ");
+		System.out.println("シトロン　　　\\250");
+		System.out.println("ショコラ　　　\\280");
+		System.out.println("ピスタージュ　\\320");
 
+	}
 }
