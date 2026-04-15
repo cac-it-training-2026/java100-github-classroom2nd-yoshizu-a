9eeb887a715286a77a679696ea1ff739655c6ad7
yoshizu <yoshizu@YOSHIZU-AEROG11.cac-k.jp.local>
2026-04-15 10:34:51 +0900

    lesson01-challenge07,08,09 完了

--- Java100_questions_cac2nd/src/lesson01/challenge09/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge09/Patisserie.java
@@ -42,6 +42,48 @@
 
 package lesson01.challenge09;
 
-public class Patisserie {
+import java.io.IOException;
 
+public class Patisserie {
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("閉店時間となりました。");
+		System.out.println("またのお越しをお待ちしております。");
+
+		System.out.println("\n売上の割合");
+		System.out.println("売上合計      \\2470");
+
+		double cito = 1250;
+		double choco = 420;
+		double pis = 800;
+		double total = 2470;
+
+		double citop = cito / total * 100;
+		double chocop = choco / total * 100;
+		double pisp = pis / total * 100;
+
+		int numcito = (int) (citop);
+		int numchoco = (int) (chocop);
+		int numpis = (int) (pisp);
+
+		System.out.println("\n内訳");
+		System.out.println("シトロン     \\1250・・・" + numcito + "% ");
+		System.out.println("ショコラ      \\420・・・" + numchoco + "%");
+		System.out.println("ピスタージュ  \\800・・・" + numpis + "%");
+
+		System.out.println("\n明日の三色マカロンの配合率が決まりました!");
+		System.out.println("\nシトロンの味・・・" + numcito + "%");
+		System.out.println("ショコラの味・・・" + numchoco + "%");
+		System.out.println("ピスタージュの味・・・" + numpis + "%");
+
+		System.out.println("\nが楽しめます!");
+
+		double kosuu = 9;
+		double dav = total / kosuu;
+		int iav = (int) (dav);
+		int nedan = (iav / 10) * 10;
+
+		System.out.println("\n値段は\\" + nedan + "です。");
+
+	}
 }
